5d7e44dff3306f46c0cab72efde46033e1d46194
SanonZel <kanatovaha@gmail.com>
2023-01-03 22:07:01 +0600

    dz6dock

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import FormTog from './FormTog.jsx'
+import CountDown from './timer'
+import Toggless from './Toggless.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <CountDown/>
+      <FormTog/>
+      <Toggless/>
+    </div>
+  )
+}
+
+export default App
